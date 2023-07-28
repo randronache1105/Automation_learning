94934c16bdc6c26767a5c17c10b6e1f1def195fc
Richard Andronache <randronache@deloittece.com>
2023-07-28 12:33:59 +0300

    Make reverse for a string

---
+++ src/makeAbba.java
@@ -0,0 +1,13 @@
+public class makeAbba {
+
+    public static void main(String[] args) {
+        //ends("Helloly);
+        System.out.println(make("ab", "cd"));
+    }
+
+    public static String make(String a, String b) {
+
+        return a + b + b + a;
+
+    }
+}
