0d817a4209b6caa5471efbd25f089fd70071ea9b
Richard Andronache <randronache@deloittece.com>
2023-07-28 15:16:21 +0300

    first and last character do not show

---
+++ src/left2.java
@@ -0,0 +1,18 @@
+public class left2 {
+
+    public static void main(String[] args) {
+        //ends("Helloly);
+        System.out.println(left("abafvva"));
+    }
+
+    public static String left(String str) {
+        String newString = new String();
+
+        if(str.length() < 2) return str;
+
+        else{
+            newString = newString + str.substring(2) + str.substring(0, 2);
+            return newString;
+        }
+    }
+}
