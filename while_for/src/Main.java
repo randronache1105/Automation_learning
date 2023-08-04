85dba1037f7d80327cbaa9665ca911372e7fb89f
Richard Andronache <randronache@deloittece.com>
2023-08-04 15:10:51 +0300

    for loop

---
+++ while_for/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 9; i >= 0; i--){
+            for(int j = 0; j <= 9 ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
