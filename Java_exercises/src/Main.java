ecabb164f0c7b763410fa188550d090597c56e8b
Richard Andronache <randronache@deloittece.com>
2023-08-07 16:37:15 +0300

    return true if the first number or last one is 6 and return true if the first number or last one from a is the same with first or second from b array

---
+++ Java_exercises/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //return true if the first number or last one is 6
+        System.out.println(firstLast6.firstLast(new int[]{1,3,4}));
+
+        //return true if the first number or last one from a is the same with first or second from b array
+        System.out.println(commonEnd.common(new int[]{1,2,3}, new int[]{7,3}));
+
+    }
+}
---
+++ Java_exercises/src/firstLast6.java
@@ -0,0 +1,8 @@
+public class firstLast6 {
+    public static boolean firstLast(int[] nums) {
+        if(nums[0] == 6 || nums[nums.length - 1] == 6)
+            return true;
+        else return false;
+
+    }
+}
