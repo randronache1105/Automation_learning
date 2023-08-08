25960b8ae81b594ba6d499e03f5ad552e7dd89d6
Richard Andronache <randronache@deloittece.com>
2023-08-08 13:39:51 +0300

    return reverse for an array

--- Java_exercises/src/Main.java
+++ Java_exercises/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //return true if the first number or last one is 6
@@ -5,6 +7,9 @@ public class Main {
 
         //return true if the first number or last one from a is the same with first or second from b array
         System.out.println(commonEnd.common(new int[]{1,2,3}, new int[]{7,3}));
+
+        // return reverse for an array
+        System.out.println(Arrays.toString(reverse3.reverse2(new int[]{1, 2, 3})));
 
     }
 }
---
+++ Java_exercises/src/reverse3.java
@@ -0,0 +1,9 @@
+public class reverse3 {
+    public static int[] reverse2(int[] nums) {
+        int[] myArray = new int[3];
+        myArray[0] = nums[2];
+        myArray[1] = nums[1];
+        myArray[2] = nums[0];
+        return myArray;
+    }
+}
